5bf02a15ab044becfaf016b5f6ceb06396935da7
Roei Rom <roeirom6@gmail.com>
2020-10-15 16:13:53 +0300

    fix(): almost finished

--- src/Missions.tsx
+++ src/Missions.tsx
@@ -9,101 +9,96 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import {setNewMission,setMissionChecked,deleteMission} from './store/Mission/missionActionCreators';
 
+// change to arrow function
+// meaningful name to the parameter
 function isEmptyOrSpaces(str:string){
+    // why the space in the regex? if you want any white space you can use \s in order to do so
+    // you can also do the regex syntax in JS, /^ *$/.test(strToTest) returns a boolean if strToTest is like the regex
     return str === null || str.match(/^ *$/) !== null;
 }
 const Missions: React.FC = (): JSX.Element => {
  
 
     const classes = useStyles();
-    const [inputValue, setInputValue] = React.useState('');
+    // add type to useState
+    const [inputValue, setInputValue] = React.useState<string>('');
+    // dont need the ref
     const textRef = React.useRef();
     const allMissions = useSelector<StoreStateType,missions>(s=>s.missions);
     
+    // JS blockes are just like java, not C#/C
+    // const func = () => {
+    //    function content.....
+    // }
+
     return (
         <div >
-            <TextField  id="outlined-basic" 
-                         autoFocus
-                        label='to do..' 
-                        variant="outlined"
-                        className={classes.editStype} 
-                        value={inputValue}
-                        inputRef={textRef}
-                        onChange={e => 
-                            {
-                                setInputValue(e.target.value);      
-                           }
-                                }
-                        onBlur={e => 
-                         {
-                             if(!isEmptyOrSpaces(inputValue))
-                                    setNewMission(inputValue)
-                              setInputValue('');
+            {/* Look at the fixed indentation */}
+            <TextField  
+                id="outlined-basic" 
+                autoFocus
+                label='to do..' 
+                variant="outlined"
+                className={classes.editStype} 
+                value={inputValue}
+                inputRef={textRef}
+                // in a function of one line you can do the following
+                onChange={e => setInputValue(e.target.value)}
+                onBlur={e => {
+                    if(!isEmptyOrSpaces(inputValue))
+                        setNewMission(inputValue)
+                    setInputValue('');
+                }}
+                onKeyUp={e => {
+                    if(e.key==='Enter') {
+                        if(!isEmptyOrSpaces(inputValue)) {
+                            setNewMission(inputValue);
+                            setInputValue('');
                         }
-                             }
-                        onKeyUp=   
-                            {e => {
-                                if(e.key==='Enter')
-                                {
-                                    if(!isEmptyOrSpaces(inputValue)){
-                                        setNewMission(inputValue);
-                                        setInputValue('');
-
-                                           
-                                    }
-                                    
-                                }
-                            }
-                                
-                           }
-                             />
-             {allMissions.list.map(function(object, i){
-               return <div   className={object.done ? classes.Done : classes.ToDo} key={i}> 
-                          <Grid container justify="flex-start" className={classes.gridAlign} >
-                          <Checkbox 
-                             color="primary"
-                            checked={object.done===true}
-                            onChange={e => {
-                            
-                                            console.log('object',object);
-                                            if(object.id)
-                                                setMissionChecked(object.id)
-                                            
-                            }}
-
-                        />
-                          {object.description }
-                          </Grid>
-                          <Grid container justify="flex-end" 
-                          >
-                          <Button 
-                          disabled= {object.done===true}
-                          onClick={()=>{
-                            if(object.id) 
-                            {
-                                
-                                setInputValue(object.description);
-                                deleteMission(object.id);
-                              //  textRef.current?.focus();
-                                
-                            }
-                            }
-                        }
-                          >edit</Button>
-                          <Button 
-                           onClick={()=>{
-                            if(object.id) 
-                                deleteMission(object.id)
-
- 
-                            }
-                        }
-                          >delete</Button>
+                    }
+                }}
+            />
+            {/* Change to arrow function */}
+            {allMissions.list.map(function(object, i){
+                return <div className={object.done ? classes.Done : classes.ToDo} key={i}> 
+                            <Grid container justify="flex-start" className={classes.gridAlign} >
+                                <Checkbox 
+                                    color="primary"
+                                    checked={object.done===true}
+                                    onChange={e => {
+                                        // remove console.log
+                                        console.log('object',object);
+                                        if(object.id)
+                                            setMissionChecked(object.id)
+                                    }}
+                                />
+                                {object.description}
+                            </Grid>
+                            <Grid container justify="flex-end">
+                                <Button 
+                                    disabled= {object.done === true}
+                                    onClick={() => {
+                                        if(object.id) {
+                                            setInputValue(object.description);
+                                            deleteMission(object.id);
+                                            // can remove the comment    
+                                            //  textRef.current?.focus();
+                                        }
+                                    }}
+                                >
+                                    edit
+                                </Button>
+                                <Button 
+                                    // the syntax of boolean && expression means that if the boolean is true then do the expression other wise do nothing
+                                    onClick={() => object.id && deleteMission(object.id)}
+                                >
+                                    delete
+                                </Button>
                           </Grid>
                       </div>
-                      
-                      ; 
-             })}
+                    }
+                )
+            }
         </div>
   )
 
